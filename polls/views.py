91b056b65d0abea9580fb472ef8dfaa129c39861
wechu07 <wechu017@gmail.com>
2023-07-04 16:09:35 +0300

    update template rendered in index view

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     # The context is a dictionary mapping template variable names to Python objects.
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# shorter way of rendering using render()
+# takes the request object as its first argument, 
+# ... a template name as its second argument and a dictionary as its optional third argument
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
