b17c7a3201bb53575908479a29a379ec597c097f
wechu07 <wechu017@gmail.com>
2023-07-07 05:56:08 -0700

    customise admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# create a model admin class, 
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
+
+# # forms with dozens of fields, you might want to split the form up into fieldsets:
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {"fields": ["question_text"]}),
+#         ("Date information", {"fields": ["pub_date"]}),
+#     ]
+
+# then pass it as the second argument to admin.site.register()
+
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+# adding choices together with questions
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -8,8 +9,16 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
